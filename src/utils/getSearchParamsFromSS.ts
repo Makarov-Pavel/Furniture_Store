aea8261503fcb7f5baf7d865b454058f0dd2d1ea
Pavel Makarov <makarov09811@mail.ru>
2023-03-01 23:26:50 +0300

    minor changes

--- src/utils/getSearchParamsFromSS.ts
+++ src/utils/getSearchParamsFromSS.ts
@@ -1,6 +1,5 @@
 interface ISearchParamsFromSS {
     page: number,
-    limit: number,
     sortBy: 'Popular' | 'Low' | 'Hight',
     type: string,
     search: string
@@ -12,14 +11,13 @@ export const getSearchParamsFromSS = (): ISearchParamsFromSS => {
 
     if(data) return {
         page: data.page,
-        limit: data.limit,
+
         sortBy: data.sortBy,
         type: data.type,
         search: data.search,
     }  
     return {
         page: 1,
-        limit: 6,
         sortBy: 'Popular',
         type: 'all',
         search: '',
